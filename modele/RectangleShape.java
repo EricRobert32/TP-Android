8610501aa8606f1ac8179017dccb7a5b3d98116d
Moonakie <darkanos00@gmail.com>
2020-05-07 14:23:59 +0200

    Create RectangleShape.java

---
+++ modele/RectangleShape.java
@@ -0,0 +1,21 @@
+package com.example.draw.Modele;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class RectangleShape implements DrawableShape {
+
+    private float coordone[];
+    public RectangleShape(float[] coordone) {
+        this.coordone =coordone;
+    }
+
+    @Override
+    public void drawShape(ShapeProperties properties, Canvas canvas) {
+        Paint noir = new Paint();
+        noir.setStrokeWidth(5);
+        noir.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(coordone[0]+properties.getX(),coordone[1]+properties.getY(),coordone[2]+properties.getX(), coordone[3]+properties.getY(),noir);
+    }
+}
